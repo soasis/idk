9ca4d55ee6b10ba3fff9f04c27fa6a5c8f763d01
ThePhD <phdofthehouse@gmail.com>
2022-10-09 05:27:16 -0400

    🛠 Use ::npos

--- source/ztd/idk/encoding_detection.c.cpp
+++ source/ztd/idk/encoding_detection.c.cpp
@@ -165,7 +165,8 @@ ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ const char* ztdc_execution_enco
 	const char* __ctype_name = setlocale(LC_CTYPE, nullptr);
 #endif
 	::std::string_view __adjusted_ctype_name(__ctype_name);
-	if (__adjusted_ctype_name.find_first_of("ANSI_") || __adjusted_ctype_name.find_first_of("USAS")) {
+	if (__adjusted_ctype_name.find_first_of("ANSI_") != std::string_view::npos
+	     || __adjusted_ctype_name.find_first_of("USAS") != std::string_view::npos) {
 		// this is an official ANSI/USAS name, return it as-is with no modifications...
 		return __adjusted_ctype_name.data();
 	}
