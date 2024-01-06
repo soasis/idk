ea272b45d09b07dbb67e5bf179416438d33ed9fe
Björkus Dorkus <phdofthehouse@gmail.com>
2024-01-06 13:00:03 -0500

    🛠 Fix const qualifications we left out

--- include/ztd/idk/encoding_name.h
+++ include/ztd/idk/encoding_name.h
@@ -45,21 +45,25 @@
 #endif
 
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal(
-     char* __left_encoding_name, char* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     const char* __left_encoding_name, const char* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal_n(size_t __left_encoding_name_size,
-     char* __left_encoding_name, size_t __right_encoding_name_size, char* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     const char* __left_encoding_name, size_t __right_encoding_name_size,
+     const char* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal_c8(
-     ztd_char8_t* __left_encoding_name, ztd_char8_t* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
-ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal_n_c8(size_t __left_encoding_name_size,
-     ztd_char8_t* __left_encoding_name, size_t __right_encoding_name_size,
-     ztd_char8_t* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     const ztd_char8_t* __left_encoding_name, const ztd_char8_t* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal_c8n(size_t __left_encoding_name_size,
+     const ztd_char8_t* __left_encoding_name, size_t __right_encoding_name_size,
+     const ztd_char8_t* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_encoding_name_equal_c8n_c8(
+     size_t __left_encoding_name_size, const ztd_char8_t* __left_encoding_name,
+     const ztd_char8_t* __right_encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_unicode_encoding_name(
-     char* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     const char* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_unicode_encoding_name_c8(
-     ztd_char8_t* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     const ztd_char8_t* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_unicode_encoding_name_n(
-     size_t __encoding_name_size, char* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     size_t __encoding_name_size, const char* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 ZTD_C_LANGUAGE_LINKAGE_I_ ZTD_IDK_API_LINKAGE_I_ bool ztdc_is_unicode_encoding_name_c8n(
-     size_t __encoding_name_size, ztd_char8_t* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
+     size_t __encoding_name_size, const ztd_char8_t* __encoding_name) ZTD_NOEXCEPT_IF_CXX_I_;
 
 #endif
